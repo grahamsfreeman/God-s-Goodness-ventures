920eba74ca4fa1052eeb31d613a3e42aab56df83
grahamsfreeman <grahamsfreeman@gmail.com>
2026-07-03 14:21:41 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,1 @@
-const SUPABASE_URL = "https://azqtynfqhpkyciehlkwh.supabase.co";
-const SUPABASE_KEY = "sb_publishable_zsAmQR8Hu00frIBoRPebCA_A6GxX8dL";
-
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
-window.supabase = supabase;
-
-alert("Supabase Connected");
+alert("Hello from supabase.js");
