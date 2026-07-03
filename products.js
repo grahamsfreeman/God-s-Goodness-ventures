89694bdde6a123609afb47b5e0897f76ea630283
grahamsfreeman <grahamsfreeman@gmail.com>
2026-07-03 14:42:37 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,19 @@
-alert("Products.js Loaded");
+async function loadProducts() {
+
+    alert("Fetching products...");
+
+    const { data, error } = await window.supabaseClient
+        .from("products")
+        .select("*");
+
+    if (error) {
+        alert("Database Error:\n" + error.message);
+        return;
+    }
+
+    alert("Products found: " + data.length);
+
+    console.log(data);
+}
+
+loadProducts();
