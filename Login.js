0d8243829c6953ac9d99daa97bccc6ec80a35480
grahamsfreeman <grahamsfreeman@gmail.com>
2026-07-03 15:22:09 +0100

    Update and rename admin.js to Login.js

---
+++ Login.js
@@ -0,0 +1,24 @@
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value;
+
+    const { error } = await window.supabaseClient.auth.signInWithPassword({
+        email,
+        password
+    });
+
+    if (error) {
+        alert(error.message);
+        return;
+    }
+
+    alert("Login Successful!");
+
+    window.location.href = "dashboard.html";
+
+});
