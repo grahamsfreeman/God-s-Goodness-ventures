594a664b27747a71728b4ae980fdaf375511205a
grahamsfreeman <grahamsfreeman@gmail.com>
2026-07-03 14:06:46 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,17 +1,1 @@
-async function test() {
 
-    alert("Starting...");
-
-    const { data, error } = await supabase
-        .from("products")
-        .select("*");
-
-    if (error) {
-        alert(error.message);
-        return;
-    }
-
-    alert(JSON.stringify(data));
-}
-
-test();
