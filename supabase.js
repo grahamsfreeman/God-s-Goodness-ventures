6631e276942642fa52ac2c3daeadbdac36c1d764
grahamsfreeman <grahamsfreeman@gmail.com>
2026-07-03 13:11:59 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,14 +1,8 @@
-alert("supabase.js loaded");
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-alert(typeof window.supabase);
+const supabaseUrl = "https://azqtynfqhpkyciehlkwh.supabase.co";
+const supabaseKey = "YOUR_ANON_KEY";
 
-const SUPABASE_URL = "https://azqtynfqhpkyciehlkwh.supabase.co";
+window.supabase = createClient(supabaseUrl, supabaseKey);
 
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6cXR5bmZxaHBreWNpZWhsa3doIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNjEzNDAsImV4cCI6MjA5ODYzNzM0MH0.VufO48qa-2z1uubZPUVgfULv66HjjklJ6gWCqr3AhZQ";
-
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
-
-alert("Connected!");
+alert("Supabase Connected!");
