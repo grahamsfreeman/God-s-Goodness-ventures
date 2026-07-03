a536659a7f0b03ee2a14f9401880a218b4af7b60
grahamsfreeman <grahamsfreeman@gmail.com>
2026-07-03 15:14:32 +0100

    Update admin.js

--- admin.js
+++ admin.js
@@ -34,3 +34,25 @@ if (loginForm) {
     });
 
 }
+// ==============================
+// CHECK LOGIN
+// ==============================
+
+async function checkAdmin() {
+
+    const {
+
+        data: { session }
+
+    } = await window.supabaseClient.auth.getSession();
+
+    if (!session) {
+
+        window.location.href = "login.html";
+        return;
+
+    }
+
+}
+
+checkAdmin();
